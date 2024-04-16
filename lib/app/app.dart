9a66b0fa0beb80b34279e607978c7e07b7d5ccd9
yousef-nageh <naghyousef@gmail.com>
2024-04-16 08:17:50 +0200

    remove block provider from main

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,17 +1,12 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../config/routes/routes.dart';
 
 import '../config/theme/app_theme.dart';
 import '../core/utils/size_config.dart';
-import '../features/home/data/repo/home_repo_impl.dart';
-import '../features/home/domain/use_cases/lower_list_use_case.dart';
-import '../features/home/domain/use_cases/upper_list_use_case.dart';
-import '../features/home/presentation/manager/lower_list_block/lower_list_cubit.dart';
-import '../features/home/presentation/manager/upper_list_block/upper_list_cubit.dart';
-import 'functions/service_locator.dart';
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -19,29 +14,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (BuildContext context) =>
-              UpperListCubit(UpperListUseCase(getIt.get<HomeRepoImpl>()))
-                ..getUpperListData()
-                ..startController(),
-        ),
-        BlocProvider(
-          create: (BuildContext context) =>
-              LowerListCubit(LowerListUseCase(getIt.get<HomeRepoImpl>()))
-                ..getLowerListData()
-                ..setScrollController(),
-        )
-      ],
-      child: MaterialApp(
+    return MaterialApp(
 
-        initialRoute: AppRoutes.splashRoutes,
-        onGenerateRoute: AppRoutes.generateRoute,
-        darkTheme: AppTheme.darkTheme,
-        themeMode: ThemeMode.dark,
-        debugShowCheckedModeBanner: false,
-      ),
+      initialRoute: AppRoutes.splashRoutes,
+      onGenerateRoute: AppRoutes.generateRoute,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
     );
   }
 }
